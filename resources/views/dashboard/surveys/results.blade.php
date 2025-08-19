e6facbb2234e5e2e237bc55ff6cb4e904856065f
elhareth0609 <elhareh0609@gmail.com>
2025-08-19 22:22:02 +0100

    remove something

--- resources/views/dashboard/surveys/results.blade.php
+++ resources/views/dashboard/surveys/results.blade.php
@@ -40,7 +40,7 @@
                         @lang('dashboard.export')
                     </button>
                     <!--begin::Menu-->
-                    <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-600 menu-state-bg-light-primary fw-bold fs-7 w-200px py-4" data-kt-menu="true">
+                    {{-- <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-600 menu-state-bg-light-primary fw-bold fs-7 w-200px py-4" data-kt-menu="true">
                         <!--begin::Menu item-->
                         <div class="menu-item px-3">
                             <a href="#" class="menu-link px-3" data-kt-ecommerce-category-filter="export_excel">
@@ -55,7 +55,7 @@
                             </a>
                         </div>
                         <!--end::Menu item-->
-                    </div>
+                    </div> --}}
                     <!--end::Menu-->
                     <!--end::Export-->
                 </div>
