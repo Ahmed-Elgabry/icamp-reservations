7fc4919367196e3a86c7ef39ae67440eb46628a7
elhareth0609 <elhareh0609@gmail.com>
2025-08-20 10:27:05 +0100

    reove /

--- resources/views/dashboard/surveys/builder.blade.php
+++ resources/views/dashboard/surveys/builder.blade.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="https://code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>\
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 	<link rel="canonical" href="{{ Request::fullUrl() }}" />
     <style>
         :root {
