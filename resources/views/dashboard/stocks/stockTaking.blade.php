7287cac75ee2b35f37ab00ed1930c79652ba1de2
mohamed sherif <mohamed.sherif.a.2022@gmail.com>
2025-09-15 21:14:18 +0300

    add some futute

--- resources/views/dashboard/stocks/stockTaking.blade.php
+++ resources/views/dashboard/stocks/stockTaking.blade.php
@@ -319,7 +319,7 @@
                             @foreach($stockTakingItems as $adj)
                                     <tr data-id="{{ $adj->id }}">
                                     <td><a href="{{ route('dashboard.stock.report', $adj->stock->id) }}">{{ $adj->stock->name ?? '-' }}</a></td>
-                                    <td>{{ ($adj->stock->percentage ?? $adj->stock->quantity) !== null ? ($adj->stock->percentage ?? $adj->stock->quantity) . '%' : '-' }}</td>
+                                    <td>{{ $adj->stock->percentage ? $adj->stock->percentage ." %": $adj->stock->quantity}}</td>
                                     <td>{{ $adj->available_quantity_after ?? '-' }}</td>
                                     <td>{{ __("dashboard.stockTaking.".$adj->type) }}</td>
                                     @if($adj->order_id)
